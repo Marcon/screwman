3f7ceb23b9b9a9e9a0cad320fb0a497965c033d9
Evgeniy <marcon@atsy.su>
2021-05-07 16:40:25 +0300

    add models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+    title = models.CharField(max_length=500, unique=True)
+    description = models.TextField(null=True)
+
+
+class DeviceType(models.Model):
+    title = models.CharField(max_length=500, unique=True)
+
+
+class Device(models.Model):
+    device_type = models.ForeignKey('DeviceType', on_delete=models.PROTECT)
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.PROTECT)
+    serial = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class Order(models.Model):
+    device = models.ForeignKey('Device', related_name='orders', on_delete=models.PROTECT)
+    accept_date = models.DateTimeField(auto_now_add=True)
+
